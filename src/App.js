d88a5707b2a6062893870fb42cca1f827155ab38
Thiago Sandi <thiago.sandi91@gmail.com>
2019-03-20 00:39:38 -0300

    Refatorando codigo

--- src/App.js
+++ src/App.js
@@ -7,22 +7,7 @@ class App extends Component {
 
   constructor(props){
       super(props)
-
       this.state = {}
-
-      // const movies = [
-      //   {id: 0, poster_src: "https://boygeniusreport.files.wordpress.com/2018/04/avengers-infinity-war3.jpg?quality=98&strip=all&w=640&h=500&crop=1", title: "Avengers: Infinity War", overview: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-      //     {id: 1, poster_src: "https://static0.cbrimages.com/wordpress/wp-content/uploads/2019/02/Avengers-Endgame-promo-costumes-header.jpg", title: "Avengers", overview: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-      // ]
-      //
-      // var movieRows = []
-      // movies.forEach((movie) =>{
-      //   const movieRow = <MovieRow movie={movie}/>
-      //    movieRows.push(movieRow)
-      // })
-      //
-      // this.state = {rows: movieRows}
-
       this.performSearch()
   }
 
@@ -30,22 +15,20 @@ class App extends Component {
     const urlString = "https://api.themoviedb.org/3/search/movie?api_key=e091522dbaa9e6c61436ed38e1d2766a&query=" + searchTerm
     $.ajax({
       url: urlString,
-        success: (searchResults) => {
-          var movieRows = []
-          const results = searchResults.results
-console.log(results)
-          results.forEach((movie) => {
+      success: (searchResults) => {
+        var movieRows = []
+        const results = searchResults.results
+        results.forEach((movie) => {
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+          const movieRow = <MovieRow key={movie.id} movie={movie} />
+          movieRows.push(movieRow)
+        })
+        this.setState({rows: movieRows})
+      },
 
-            movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
-            const movieRow = <MovieRow key={movie.id} movie={movie} />
-            movieRows.push(movieRow)
-          })
-
-          this.setState({rows: movieRows})
-        },
-        error: (xhr, status, err) => {
-          console.error("Failer")
-        }
+      error: (xhr, status, err) => {
+        console.error("Failer")
+      }
     })
   }
 
